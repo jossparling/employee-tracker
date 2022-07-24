4cb4da2b84184018b64aa5ac7dd831fd07edc348
jossparling <jossparling@gmail.com>
2022-07-23 20:43:14 -0500

    added inquirer package

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-const cTable = require('console.table');
-const db = require('./db/connection');
+// const cTable = require('console.table');
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'department',
+        message: 'What is the department name? (Required)'
+      },
+      {
+        type: 'input',
+        name: 'role',
+        message: 'Enter the role (Required)'
+      }
+    ]);
+};
+
+promptUser();
